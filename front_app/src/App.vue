2052d32db67f1edd43aee179c816a889ca900d7c
Mariusz Wierzbicki <mariusz.wierzbicki@gmail.com>
2018-10-22 15:28:45 +0200

    add gauge component (#4)

--- front_app/src/App.vue
+++ front_app/src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="item in items">
+      <Gauge :value="item.value"></Gauge>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Gauge from './components/Gauge.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+      Gauge
+  },
+    data() {
+        return {
+            test: 'test',
+            items: [
+                {value: 10},
+                {value: 50}
+            ]
+        }
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ front_app/src/components/Gauge.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <GChart
+                :settings="{ packages: ['gauge'] }"
+                type="Gauge"
+                :data="chartData"
+                :options="chartOptions"
+        />
+    </div>
+</template>
+
+<script>
+    import { GChart } from 'vue-google-charts'
+    export default {
+        name: "Gauge",
+        components: {
+            GChart
+        },
+        props: ['value'],
+        data() {
+            return {
+                chartOptions: {
+                    width: '100%', height: '100%',
+                    redFrom: 90, redTo: 100,
+                    yellowFrom:75, yellowTo: 90,
+                    minorTicks: 5
+                }
+            };
+        },
+        computed: {
+            chartData: function() {
+              return [
+                  ['Label', 'Value'],
+                  ['temp (°C)', this.value],
+              ]
+            }
+        }
+    }
+</script>
+
